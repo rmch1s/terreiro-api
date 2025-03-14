f2c8699441b452d14ca0bab992c11d3e1030d1f5
Ramon Maximo <rmch1s.hotmail.com>
2025-03-14 02:40:13 -0300

    adding user role tests

---
+++ Terreiro.Tests/Entities/UserRoleTest.cs
@@ -0,0 +1,27 @@
+﻿using Bogus;
+using FluentAssertions;
+using Terreiro.Domain.Entities;
+
+namespace Terreiro.Tests.Entities;
+
+public class UserRoleTest
+{
+    private readonly Faker _faker = new("pt_BR");
+
+    [Fact]
+    [Trait("Category", "UserRole")]
+    [Trait("Method", "Constructor")]
+    public void Constructor_GivenAllParameters_ThenSetPropertiesCorrectly()
+    {
+        //Arrange
+        var expectedUserId = _faker.Random.Int();
+        var expectedRoleId = _faker.Random.Int();
+
+        // Act
+        var user = new UserRole(expectedUserId, expectedRoleId);
+
+        // Assert
+        user.UserId.Should().Be(expectedUserId);
+        user.RoleId.Should().Be(expectedRoleId);
+    }
+}
