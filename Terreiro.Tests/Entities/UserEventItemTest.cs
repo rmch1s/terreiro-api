3e77b03e9d3d2440e24824c6a85ab5132063d35c
Ramon Maximo <rmch1s.hotmail.com>
2025-03-14 02:38:48 -0300

    adding user event item tests

---
+++ Terreiro.Tests/Entities/UserEventItemTest.cs
@@ -0,0 +1,27 @@
+﻿using Bogus;
+using FluentAssertions;
+using Terreiro.Domain.Entities;
+
+namespace Terreiro.Tests.Entities;
+
+public class UserEventItemTest
+{
+    private readonly Faker _faker = new("pt_BR");
+
+    [Fact]
+    [Trait("Category", "UserEventItem")]
+    [Trait("Method", "Constructor")]
+    public void Constructor_GivenAllParameters_ThenSetPropertiesCorrectly()
+    {
+        //Arrange
+        var expectedUserId = _faker.Random.Int();
+        var expectedEventItemId = _faker.Random.Int();
+
+        // Act
+        var user = new UserEventItem(expectedUserId, expectedEventItemId);
+
+        // Assert
+        user.UserId.Should().Be(expectedUserId);
+        user.EventItemId.Should().Be(expectedEventItemId);
+    }
+}
