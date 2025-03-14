cd8b264b4f4ee3af3f006dc5f75d435e4563af38
Ramon Maximo <rmch1s.hotmail.com>
2025-03-14 02:07:28 -0300

    adding role tests

---
+++ Terreiro.Tests/Entities/RoleTest.cs
@@ -0,0 +1,64 @@
+﻿using Bogus;
+using FluentAssertions;
+using Terreiro.Domain.Entities;
+using Terreiro.Tests.Fixtures.Entities;
+
+namespace Terreiro.Tests.Entities;
+
+public class RoleTest
+{
+    private readonly Faker _faker = new("pt_BR");
+
+    [Fact]
+    [Trait("Category", "Role")]
+    [Trait("Method", "Constructor")]
+    public void Constructor_GivenAllParameters_ThenSetPropertiesCorrectly()
+    {
+        //Arrange
+        var expectedName = _faker.Person.FirstName;
+        var expectedDescription = _faker.Random.String().OrNull(_faker);
+
+        // Act
+        var role = new Role(expectedName, expectedDescription);
+
+        // Assert
+        role.Name.Should().Be(expectedName);
+        role.Description.Should().Be(expectedDescription);
+        role.CreatedAt.Should().BeCloseTo(DateTime.UtcNow, TimeSpan.FromSeconds(1));
+    }
+
+    [Fact]
+    [Trait("Category", "Role")]
+    [Trait("Method", "Update")]
+    public void Update_GivenAllParameters_ThenSetPropertiesCorrectly()
+    {
+        // Arrange
+        var role = RoleFixture.GenerateRoles(1).First();
+
+        var expectedName = _faker.Person.FirstName;
+        var expectedDescription = _faker.Random.String().OrNull(_faker);
+
+        //Act
+        role.Update(expectedName, expectedDescription);
+
+        //Assert
+        role.Name.Should().Be(expectedName);
+        role.Description.Should().Be(expectedDescription);
+        role.UpdatedAt.Should().BeCloseTo(DateTime.UtcNow, TimeSpan.FromSeconds(1));
+    }
+
+    [Fact]
+    [Trait("Category", "Role")]
+    [Trait("Method", "SetDeletedAt")]
+    public void SetDeletedAt_WhenIsCalled_SetDeletedAtCorrectly()
+    {
+        //Arrange
+        var role = RoleFixture.GenerateRoles(1).First();
+
+        //Act
+        role.SetDeletedAt();
+
+        //Assert
+        role.DeletedAt.Should().BeCloseTo(DateTime.UtcNow, TimeSpan.FromSeconds(1));
+    }
+}
---
+++ Terreiro.Tests/Fixtures/Entities/RoleFixture.cs
@@ -0,0 +1,14 @@
+﻿using Bogus;
+using Terreiro.Domain.Entities;
+
+namespace Terreiro.Tests.Fixtures.Entities;
+
+public class RoleFixture
+{
+    public static IEnumerable<Role> GenerateRoles(int quantity) =>
+        new Faker<Role>()
+            .CustomInstantiator(f => new(
+                f.Person.FirstName,
+                f.Random.String().OrNull(f))
+            ).Generate(quantity);
+}
