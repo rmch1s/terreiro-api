fb1d5c9511f3b70faae2ec617a6d6a9c13726299
Ramon Maximo <rmch1s.hotmail.com>
2025-03-14 00:08:20 -0300

    adding event tests

---
+++ Terreiro.Tests/Entities/EventTest.cs
@@ -0,0 +1,79 @@
+﻿using Bogus;
+using FluentAssertions;
+using Terreiro.Domain.Entities;
+using Terreiro.Domain.ValueObjects;
+using Terreiro.Tests.Fixtures;
+
+namespace Terreiro.Tests.Entities;
+
+public class EventTest
+{
+    private readonly Faker _faker = new("pt_BR");
+
+    [Fact]
+    [Trait("Category", "Event")]
+    [Trait("Method", "Constructor")]
+    public void Constructor_GivenAllParameters_ThenSetPropertiesCorrectly()
+    {
+        //Arrange
+        var expectedName = _faker.Person.FirstName;
+        var expectedPeriod = new Period(_faker.Date.Future(), _faker.Date.Future());
+        var expetedItems = EventItemFixture.GenerateEventItems(1).ToArray();
+        var expectedDescription = _faker.Random.String().OrNull(_faker);
+
+        // Act
+        var @event = new Event(expectedName, expectedPeriod, expetedItems, expectedDescription);
+
+        // Assert
+        @event.Name.Should().Be(expectedName);
+        @event.Period.Should().Be(expectedPeriod);
+        @event.Items.Should().Equal(expetedItems);
+        @event.Description.Should().Be(expectedDescription);
+        @event.CreatedAt.Should().BeCloseTo(DateTime.UtcNow, TimeSpan.FromSeconds(1));
+    }
+
+    [Fact]
+    [Trait("Category", "Event")]
+    [Trait("Method", "Update")]
+    public void Update_GivenAllParameters_ThenSetPropertiesCorrectly()
+    {
+        // Arrange
+        var expectedName = _faker.Person.FirstName;
+        var expectedPeriod = new Period(_faker.Date.Future(), _faker.Date.Future());
+        var expectedDescription = _faker.Random.String().OrNull(_faker);
+
+        var name = _faker.Person.FirstName;
+        var period = new Period(_faker.Date.Future(), _faker.Date.Future());
+        var description = _faker.Random.String().OrNull(_faker);
+        var items = EventItemFixture.GenerateEventItems(1).ToArray();
+
+        //Act
+        var @event = new Event(name, period, items, description);
+        @event.Update(expectedName, expectedPeriod, expectedDescription);
+
+        //Assert
+        @event.Name.Should().Be(expectedName);
+        @event.Period.Should().Be(expectedPeriod);
+        @event.Description.Should().Be(expectedDescription);
+        @event.UpdatedAt.Should().BeCloseTo(DateTime.UtcNow, TimeSpan.FromSeconds(1));
+    }
+
+    [Fact]
+    [Trait("Category", "Event")]
+    [Trait("Method", "SetDeletedAt")]
+    public void SetDeletedAt_WhenIsCalled_SetDeletedAtCorrectly()
+    {
+        //Arrange
+        var name = _faker.Person.FirstName;
+        var period = new Period(_faker.Date.Future(), _faker.Date.Future());
+        var description = _faker.Random.String().OrNull(_faker);
+        var items = EventItemFixture.GenerateEventItems(1).ToArray();
+
+        //Act
+        var @event = new Event(name, period, items, description);
+        @event.SetDeletedAt();
+
+        //Assert
+        @event.DeletedAt.Should().BeCloseTo(DateTime.UtcNow, TimeSpan.FromSeconds(1));
+    }
+}
---
+++ Terreiro.Tests/Fixtures/EventItemFixture.cs
@@ -0,0 +1,15 @@
+﻿using Bogus;
+using Terreiro.Domain.Entities;
+
+namespace Terreiro.Tests.Fixtures;
+
+public class EventItemFixture
+{
+    public static IEnumerable<EventItem> GenerateEventItems(int quantity) =>
+        new Faker<EventItem>()
+            .CustomInstantiator(f => new EventItem(
+                f.Person.FirstName,
+                f.Random.Int(),
+                f.Random.Int())
+            ).Generate(quantity);
+}
